a73100133490e37abd829f868e32201f0d5e7931
michal <m.a.p.zaleski@protonmail.com>
2020-03-24 21:22:57 +0100

    first change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
-kivy.require('1.0.6') # replace with your current kivy version !
+kivy.require('1.11.1')  # replace with your current kivy version !
 
 
 class MyApp(App):
